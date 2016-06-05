2f1438c71dc5cf10cb64f114b852eee20a4942ee
alexzwir <alexzwir@gmail.com>
2016-06-05 19:00:54 -0300

    Expections and functions exercises

---
+++ control-flow/functions.py
@@ -0,0 +1,40 @@
+# def add(a,b):
+# 	return a + b
+
+# x = add(a=1000000,b=2)
+# print(x)
+
+
+# m = ['Alex', 29, 'São Paulo - SP', 'Brasil', 'Av. Paulista']
+
+# print(m) 
+
+# x = input('Escreva algo que iremos colocar na lista a seguir: ')
+
+# def append_zero(lst=None):
+# 	if lst == None:
+# 		lst=[]
+# 	lst.append(x)
+# 	return lst
+
+# m_new = []
+# m_new = append_zero(m)
+
+# print(m_new)
+def alex_tosquera(a,b):
+	return a+b
+
+
+
+def chubaka():
+	try: 
+		x = alex_tosquera(123123,{'Alex':'<Toq></Toq>'})
+		print(x)
+	except:
+		print('error')
+	print('Existing the code')
+
+chubaka()
+x = alex_tosquera(2389127398721873871283,98172798127371293798127)
+print(x)
+
