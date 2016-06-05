4f3d81cdae6bfc6535c67473d18a110e7e8e9193
alexzwir <alexzwir@gmail.com>
2016-06-05 13:46:34 -0300

    Using the break and continue command

--- control-flow/while.py
+++ control-flow/while.py
@@ -7,6 +7,17 @@ input() --> It's a function that you capture what the user type in.
 
 != --> this command means 'not equal to'.
 
+break --> This command you could stop the loop, and move to the next line of the code OUT OF THE LOOP
+
+continue --> This command will jumps back to the loop again. Eg.
+	x = 0
+	while x < 5:
+		print(x)
+		x += 1
+		continue
+		print('This code won't be activate') 
+
+
 """
 
 """
@@ -15,11 +26,20 @@ x = 0
 while x < 5:
 	print(x)
 	x += 1 #This notation is a shortcut for writing 'x = x + 1'
-	
+
 """
 
-print("Enter your relative names and yours. If you want to stop, just type 'stop'.")
-s = ''
+# print("Enter your relative names and yours. If you want to stop, just type 'stop'.")
+# s = ''
 
-while s != 'stop':
-	s = input('Enter you name here: ')
+# while s != 'stop':
+# 	s = input('Enter you name here: ')
+
+x = 0
+while x < 5:
+	print(x)
+	x += 1
+	print(x)
+	break
+	# continue
+	print("This code won't be activate")	
